36ed8ce7614edb4ee11c62ad6b6510bc7a969116
loki79uk <loki.uk@gmail.com>
2023-12-16 09:32:40 +0000

    Do registration after validateTypes

--- register.lua
+++ register.lua
@@ -2,14 +2,17 @@
 -- BULK FILL MOD
 -- ============================================================= --
 bulkFillREGISTER = {}
-
 g_specializationManager:addSpecialization('bulkFill', 'BulkFill', Utils.getFilename('BulkFill.lua', g_currentModDirectory), nil)
 
-for vehicleName, vehicleType in pairs(g_vehicleTypeManager.types) do
-	if  SpecializationUtil.hasSpecialization(FillUnit, vehicleType.specializations) and
-		SpecializationUtil.hasSpecialization(FillVolume, vehicleType.specializations) and
-		SpecializationUtil.hasSpecialization(Cover, vehicleType.specializations)
-	then
-		g_vehicleTypeManager:addSpecialization(vehicleName, g_currentModName .. '.bulkFill')
+TypeManager.validateTypes = Utils.appendedFunction(TypeManager.validateTypes, function(self)
+	if self.typeName == "vehicle" then
+		for vehicleName, vehicleType in pairs(g_vehicleTypeManager.types) do
+			if  SpecializationUtil.hasSpecialization(FillUnit, vehicleType.specializations) and
+				SpecializationUtil.hasSpecialization(FillVolume, vehicleType.specializations) and
+				SpecializationUtil.hasSpecialization(Cover, vehicleType.specializations)
+			then
+				g_vehicleTypeManager:addSpecialization(vehicleName, BulkFill.modName .. '.bulkFill')
+			end
+		end
 	end
-end
+end)
